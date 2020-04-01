fd9cf9e7ba8338237318c02fa080cd5045b2937c
sun-shadow <sun-shadow@gmail.com>
2020-04-01 15:49:43 +0800

    插件的实现

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HelloWorldPlugin = require('./plugins/basic.js')
+const FileListPlugin = require('./plugins/file-list-plugin.js')
+const CleanPlugin = require('./plugins/clean-plugin.js')
+const CountTimePlugin = require('./plugins/count-time.js')
 
+const EmptyPlugin = require('empty-webpack-plugin');
 
 module.exports = {
   resolveLoader: {
@@ -39,6 +44,10 @@ module.exports = {
       filename: 'index.html',
       template: './src/views/index.html',
       title: 'haha'
-    })
+		}),
+		new HelloWorldPlugin({a: 1}),
+		new FileListPlugin(),
+		new EmptyPlugin({exclude: 'a'}),
+		new CountTimePlugin()
 	]
 }
